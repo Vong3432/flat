986aee38ff45c22464a52cc1f599eb3c561e0bc3
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-10-10 21:17:56 +0800

    a

--- index.js
+++ index.js
@@ -6,3 +6,33 @@ function openNavBar() {
         x.className = "topnav";
     }
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function toggleBtn(x) {
+	if( x == document.getElementById("dropbtn1"))
+    {
+    	document.getElementById("buyToggleContent").classList.toggle("show");
+    }
+
+    else if ( x == document.getElementById("dropbtn2") ) 
+    {
+    	document.getElementById("sellToggleContent").classList.toggle("show");
+    }
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
